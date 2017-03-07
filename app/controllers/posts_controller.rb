b1fd4dd413504a2e851df936ad6e8308d1036eb6
lihantao <htl00001111@163.com>
2017-03-07 22:12:37 +0800

    use simple_format

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
 
-  before_action :authenticate_user!, :only => [:new, :create]
+  before_action :authenticate_user!, :only => [:new, :create, :destroy]
 
   def new
     @group = Group.find(params[:group_id])
